7dba0b4d0c0f1537b8a6d4eb06e713485f90c355
Mike Kirby <mike.kirby@guildifai.com>
2025-06-27 13:30:07 -0700

    refactor(cleanup): remove unused Tailwind classes and improve formatting
    - Removed unused flex, flex-col, flex-row Tailwind classes from React component
    - Layout is properly handled by vanilla CSS in content.css file
    - Improved JSX indentation and spacing for better readability
    - Added space in "Powered by " text for cleaner display

--- src/content/OverlayComponent.tsx
+++ src/content/OverlayComponent.tsx
@@ -18,42 +18,43 @@ const OverlayComponent: React.FC<OverlayComponentProps> = ({ data }) => {
   const flagUrl = `https://purecatamphetamine.github.io/country-flag-icons/3x2/${flagCode}.svg`;
 
   return (
-    <div className="overlay-content flex flex-col">
-
-        <div className="overlay-content-main flex flex-row">
-      {/* Flag */}
-      <div className="flag-container">
-        <img 
-          src={flagUrl}
-          alt={flagTitle}
-          title={flagTitle}
-        />
+    <div className="overlay-content">
+      <div className="overlay-content-main">
+        {/* Flag */}
+        <div className="flag-container">
+          <img 
+            src={flagUrl}
+            alt={flagTitle}
+            title={flagTitle}
+          />
+        </div>
+        
+        {/* Content */}
+        <div className="content-area">
+          {/* Attributes */}
+          <ul className="attributes-list">
+            {data.attributes.map((attribute, index) => (
+              <li key={index}>
+                {attribute}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       
-      {/* Content */}
-      <div className="content-area">
-        {/* Attributes */}
-        <ul className="attributes-list">
-          {data.attributes.map((attribute, index) => (
-            <li key={index}>
-              {attribute}
-            </li>
-          ))}
-        </ul>
-      </div>
-      </div>
-        {/* Powered by link */}
-        <div className="powered-by-container">
+      {/* Powered by link */}
+      <div className="powered-by-container">
         <div className="powered-by">
-            <span>Powered by</span>           
-            <a
-                href="https://usaboycott.ca"
-                target="_blank"
-                rel="noopener noreferrer"
-            >usaboycott.ca
-            </a>
+          <span>Powered by </span>
+          <a
+            href="https://usaboycott.ca"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            usaboycott.ca
+          </a>
         </div>
-        </div>
+      </div>
     </div>
   );
 };
